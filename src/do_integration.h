3ae2fb73e073745e11b40a11004c7ab59853ac74
Allie Dabney <adabney3@vols.utk.edu>
2025-04-24 14:12:15 +0000

    new header file

---
+++ src/do_integration.h
@@ -0,0 +1,11 @@
+/*
+ *	integrating the change in popualtions of isotopes over timesteps
+ *
+ *	this function is going to calculate the maximum stable forward euler timestep (dt_FE) and the time timestep used for the
+ *	explicit assymptotic (EA)
+ *
+ *	this function will update populations using midpoint
+ *
+ */
+
+
