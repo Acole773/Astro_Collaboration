39755e91b59e8507ecd3cbb98ac17f739e2abc0d
Allie Dabney <adabney3@vols.utk.edu>
2025-04-24 14:43:40 +0000

    new header file

---
+++ src/network_cal.h
@@ -0,0 +1,13 @@
+/*
+ *	pull in information from Einstein toolkit (mass fraction {X_i}, total mass (M_total, temperature, and density)
+ *
+ *	create a while loop where t<t_final
+ *	this while loop will update the popluations (y_old), the partition function (PF), compute reaction 
+ *	fluxes, and get the max timestep
+ *
+ *	for integration step: 
+ *	-sum mass fractions
+ *	-network mass difference
+ *
+ */
+
